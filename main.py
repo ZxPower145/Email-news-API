5cc73fa0f30ad29dc4b84d180ab0c6c35efeb21e
ZxPower145 <zxpower@yahoo.com>
2023-05-21 21:53:47 +0200

    Included streamlit and added an UI

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 import requests as req
 import send_email as se
-
-topic = "tesla"
+import streamlit as st
 
 api_key = "79d1f6c78da344299c31ce709ef97daf"
 
-url = f"https://newsapi.org/v2/everything?q={topic}&" \
-      "from=2023-04-21&" \
-      "sortBy=publishedAt&" \
-      f"apiKey={api_key}&" \
-      "language=en"
-
-# made a request
-request = req.get(url)
-
-# Get a dictionary with data
-content = request.json()
-
 body = ""
 
-# Access the article titles and description
-for article in content["articles"][:20]:
-    if article["title"] is not None:
-        body = body + article["title"] + "\n" \
-               + article["description"] + "\n" \
-               + article["url"] + 2*"\n"
+# UI
+st.header("Sign up for a free newsletter :")
 
-message = f"""\
-Subject: {topic.capitalize()} News Letter
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address: ")
+    topic = st.selectbox('Select a topic for the newsletter: ', ('Tesla', 'Business', 'Entertainment',
+                                                                 'General', 'Health', 'Science', 'Sports',
+                                                                 'Technology'))
 
-{body}
+# The url for the news API
+    url = f"https://newsapi.org/v2/everything?q={topic.lower()}&" \
+          "from=2023-04-21&" \
+          "sortBy=publishedAt&" \
+          f"apiKey={api_key}&" \
+          "language=en"
 
-"""
-message = message.encode("utf-8")
-se.send_email(message)
+    # made a request
+    request = req.get(url)
+
+    # Get a dictionary with data
+    content = request.json()
+
+    sub_btn = st.form_submit_button("Send")
+    for article in content["articles"][:20]:
+        if article["title"] is not None:
+            body = body + article["title"] + "\n" \
+                   + article["description"] + "\n" \
+                   + article["url"] + 2 * "\n"
+
+    message = f"""\
+    Subject: {topic.capitalize()} News Letter
+
+    {body}
+
+    """
+    message = message.encode("utf-8")
+    if sub_btn:
+        se.send_email(user_email, message)
+        st.info("Email sent successfully!")
+
--- send_email.py
+++ send_email.py
@@ -3,14 +3,12 @@ import ssl
 import os
 
 
-def send_email(message):
+def send_email(receiver, message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "costinbogdan145@gmail.com"
     password = os.getenv("PASS")
-
-    receiver = "costinbogdan145@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
